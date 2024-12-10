376aa2e67ad948f09529e99bfe7e40595cbc4ea3
gojaguarco <gojaguarco@gmail.com>
2024-12-09 21:53:04 -0500

    modal fix

--- app/(Pages)/_components/Modal.tsx
+++ app/(Pages)/_components/Modal.tsx
@@ -28,8 +28,8 @@ const Modal = ({
   }, []);
 
   return(
-    <section onClick={handleOpenChange} className="fixed z-[100] top-0 left-0 w-screen h-screen bg-black bg-opacity-50 backdrop-blur-sm default-paddings">
-      <div onClick={stopPropagation} className="w-full max-w-screen-xl flex items-center justify-center">
+    <section onClick={handleOpenChange} className="fixed z-[100] top-0 left-0 w-screen h-screen bg-black bg-opacity-50 backdrop-blur-sm default-paddings flex items-center justify-center">
+      <div onClick={stopPropagation} className="flex items-center justify-center">
       <svg onClick={handleOpenChange} xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className=" cursor-pointer absolute top-3 right-3 lg:top-20 lg:right-20 w-10 h-10 hover:drop-shadow hover:-translate-y-0.5">
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M10 10l4 4m0 -4l-4 4" />
