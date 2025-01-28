b462069a274cd4578463cf04452d074d324bc1cc
Julian Bustos <julian.m.bustos@gmail.com>
2025-01-28 10:58:39 -0500

    Refactors surface types schema to include Colombian price component and improves field definitions

--- sanity/schemaTypes/surfacesTypes.ts
+++ sanity/schemaTypes/surfacesTypes.ts
@@ -1,80 +1,85 @@
-import { defineField, defineType } from "sanity";
+import { defineArrayMember, defineField, defineType } from "sanity";
 import { ImageSchema } from "./generalSchemas";
 import ColombianPrice from "../components/ColombianPrice";
 
-
 export const surfaceTypesType = defineType({
-    name: 'surfaceTypes',
-    title: 'Tipos de Superficies',
-    type: 'document',
-    fields: [
-        defineField({
-            name: 'title',
-            title: 'Nombre del tipo de Superficie',
-            type: 'string',
-            validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-            name: 'description',
-            title: 'Descripción del tipo de Superficie',
-            type: 'blockContent',
-            validation: (Rule) => Rule.required(),
-        }),
-        ImageSchema,
-    ]
-})
+  name: "surfaceTypes",
+  title: "Tipos de Superficies",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Nombre del tipo de Superficie",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "description",
+      title: "Descripción del tipo de Superficie",
+      type: "blockContent",
+      validation: (Rule) => Rule.required(),
+    }),
+    ImageSchema,
+  ],
+});
 
 export const surfaceType = defineType({
-    name: 'surface',
-    title: 'Superficie',
-    type: 'document',
-    fields: [
-        defineField({
-            name: "available",
-            title: "Disponible",
-            type: "boolean",
+  name: "surface",
+  title: "Superficie",
+  type: "document",
+  fields: [
+    defineField({
+      name: "available",
+      title: "Disponible",
+      type: "boolean",
+    }),
+    defineField({
+      name: "title",
+      title: "Nombre",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "code",
+      title: "Código del Material",
+      type: "number",
+    }),
+    ImageSchema,
+    defineField({
+      name: "type",
+      title: "Tipo de Superficie",
+      type: "reference",
+      to: { type: "surfaceTypes" },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "caliber",
+      title: "Calibre",
+      type: "string",
+    }),
+    defineField({
+      name: "price",
+      title: "Precio m2",
+      type: "string",
+      components: { input: ColombianPrice },
+    }),
+    defineField({
+      name: "formatos",
+      title: "Formatos",
+      type: "array",
+      of: [
+        defineArrayMember({
+          name: "formato",
+          title: "Formato",
+          type: "reference",
+          to: { type: "surfaceFormat" },
         }),
-        defineField({
-            name: 'title',
-            title: 'Nombre',
-            type: 'string',
-            validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-            name: 'code',
-            title: 'Código del Material',
-            type: 'number',
-        }),
-        ImageSchema,
-        defineField({
-            name: 'type',
-            title: 'Tipo de Superficie',
-            type: 'reference',
-            to: { type: 'surfaceTypes' },
-            validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-            name: "caliber",
-            title: "Calibre",
-            type: "string",
-        }),
-        defineField({
-            name: 'price',
-            title: 'Precio m2',
-            type: 'string',
-            components: { input: ColombianPrice },
-        }),
-        defineField({
-            name: "formatos",
-            title: "Formatos",
-            type: "array",
-            of: [{ type: "string" }],
-        }),
-        defineField({
-            name: 'description',
-            title: 'Descripción',
-            type: 'text',
-        }),
-  
-    ]
-})
+      ],
+    }),
+    defineField({
+      name: "description",
+      title: "Descripción",
+      type: "text",
+    }),
+  ],
+});
