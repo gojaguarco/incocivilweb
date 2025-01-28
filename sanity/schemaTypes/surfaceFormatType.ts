e6fc2995f6c98098ef311793b80ccb92c1235afd
Julian Bustos <julian.m.bustos@gmail.com>
2025-01-27 20:32:08 -0500

    Adds the correct items and format for the surfaceFormat document

--- sanity/schemaTypes/surfaceFormatType.ts
+++ sanity/schemaTypes/surfaceFormatType.ts
@@ -1,27 +1,41 @@
 import { defineField, defineType } from "sanity";
+import SizeIcon from "../components/icons/SizeIcon";
 
 export const surfaceFormatType = defineType({
   name: "surfaceFormat",
   title: "Formatos de Superficie",
   type: "document",
+  icon: SizeIcon,
   fields: [
     defineField({
-      name: "title",
-      title: "Título",
-      type: "string",
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      name: "wide",
+      name: "width",
       title: "Ancho",
       description: "En (cms)",
       type: "number",
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: "height",
       title: "Alto",
       description: "En (cms)",
       type: "number",
+      validation: (Rule) => Rule.required(),
     })
-  ]
+  ],
+  preview: {
+    select: {
+      width: "width",
+      height: "height",
+    },
+    prepare({ width, height }) {
+      if (!width || !height) {
+        return {
+          title: "Formato de Superficie",
+        };
+      }
+      return {
+        title: `${width} cms x ${height} cms`,
+      };
+    },
+  }
 });
