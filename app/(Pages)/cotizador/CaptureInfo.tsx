bfc8ef8a794ede7c3871d7d430d97a457535f190
Julian Bustos <julian.m.bustos@gmail.com>
2025-02-11 17:52:35 -0500

    Adds the capture info modal

--- app/(Pages)/cotizador/CaptureInfo.tsx
+++ app/(Pages)/cotizador/CaptureInfo.tsx
@@ -11,13 +11,17 @@ const CaptureInfo = ({
   captureInfoOpen,
   surfaceFormats,
   setShowTotal,
-  formTitle, 
-  successMessage
+  formTitle,
+  successMessage,
 }: {
   surfaceFormats: {
     [surfaceId: string]: SurfaceToSendAdminEmail;
-  }
-  createQueryString: (name: string, value: string, action: 'add' | 'remove' | 'replace') => string;
+  };
+  createQueryString: (
+    name: string,
+    value: string,
+    action: "add" | "remove" | "replace"
+  ) => string;
   captureInfoOpen: boolean;
   setShowTotal: Dispatch<SetStateAction<boolean>>;
   formTitle: string;
@@ -28,9 +32,7 @@ const CaptureInfo = ({
   let total = 0;
 
   for (const surfaceFormatIn in surfaceFormats) {
-
-
-    const surfaceFormat = surfaceFormats[surfaceFormatIn]
+    const surfaceFormat = surfaceFormats[surfaceFormatIn];
     total += surfaceFormat.totalSurface;
   }
   return (
@@ -45,23 +47,29 @@ const CaptureInfo = ({
       {captureInfoOpen && (
         <section
           onClick={() => {
-            router.push(`?${createQueryString("capture-info", "true", "remove")}`, { scroll: false })
+            router.push(
+              `?${createQueryString("capture-info", "true", "remove")}`,
+              { scroll: false }
+            );
           }}
           className="fixed top-0 z-[200] left-0 w-screen h-full bg-black bg-opacity-50 flex justify-center items-center"
         >
-
           <LightCard
-            onClick={e => e.stopPropagation()}
+            onClick={(e) => e.stopPropagation()}
             className="max-w-screen-sm bg-light-dark p-10 relative z-[200] rounded-xl px-10 flex flex-col w-[85dvw] mx-auto gap-5"
           >
-            <CaptureForm formTitle={formTitle} successMessage={successMessage} setShowTotal={setShowTotal} selectedFormats={surfaceFormats} totalToShow={total} />
+            <CaptureForm
+              formTitle={formTitle}
+              successMessage={successMessage}
+              setShowTotal={setShowTotal}
+              selectedFormats={surfaceFormats}
+              totalToShow={total}
+            />
           </LightCard>
         </section>
       )}
     </>
   );
-}
+};
 
 export default CaptureInfo;
-
-
