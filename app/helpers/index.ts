eae181065a194918e87683970cfb9cb95ec31ea3
Julian Bustos <julian.m.bustos@gmail.com>
2025-02-11 17:22:44 -0500

    fixes build errors

---
+++ app/helpers/index.ts
@@ -0,0 +1,15 @@
+export const numberToColombianPriceString = function (number: number) {
+  const numStr = number.toString();
+  let result = "";
+  let count = 0;
+
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    count++;
+    result = numStr[i] + result;
+    if (count === 3 && i !== 0) {
+      result = "." + result;
+      count = 0;
+    }
+  }
+  return `$${result}`;
+};
