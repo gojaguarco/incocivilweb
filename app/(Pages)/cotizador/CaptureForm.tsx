cd76aeb93b3494b660c5bde248badb08fb9cba72
Julian Bustos <julian.m.bustos@gmail.com>
2025-02-13 18:04:58 -0500

    Syncs the form data with local storage and adds the action with only error handling

---
+++ app/(Pages)/cotizador/CaptureForm.tsx
@@ -0,0 +1,152 @@
+import { numberToColombianPriceString } from "@/app/helpers";
+import { ComponentPropsWithoutRef, useActionState, useEffect, useState } from "react";
+import { cn } from "../_lib/cn";
+import { captureInfoAction } from "./captureInfoAction";
+
+
+
+// const useLocalStorage = (key: string, initialValue: any) => {
+//   const storedValue = localStorage.getItem(`incocivil-${key}`);
+//   const initial = storedValue ? JSON.parse(storedValue) : initialValue;
+//   const [value, setValue] = useState(initial);
+
+//   useEffect(() => {
+//     localStorage.setItem(`incocivil-${key}`, JSON.stringify(value));
+//   }, [`incocivil-${key}`, value]);
+
+//   return [value, setValue];
+// };
+
+
+const CaptureForm = ({ totalToShow }: {
+  totalToShow: number;
+}) => {
+
+  const [state, formAction, isPending] = useActionState(captureInfoAction, {
+    success: false,
+    errors: null,
+  });
+
+  const [data, setData] = useState(() => {
+    const savedData = localStorage.getItem('formData');
+    return savedData ? JSON.parse(savedData) : {
+      nombre: '',
+      apellido: '',
+      email: '',
+      telefono: '',
+      mensaje: '',
+    };
+  });
+
+  useEffect(() => {
+    if (data) {
+      localStorage.setItem('formData', JSON.stringify(data));
+    }
+  }, [data]); 
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const value = e.target.value;
+
+    const inputName = e.target.name;
+
+    if (!value || !inputName) return;
+
+    setData((prev: {
+      nombre: string;
+      apellido: string;
+      email: string;
+      telefono: string;
+      mensaje: string;
+    }) => ({
+      ...prev,
+      [inputName]: value
+    }))
+  };
+
+
+
+  return (
+    <form className="flex flex-col gap-5">
+      <h1>¡Casi está lista tu cotización!</h1>
+      <p>El valor
+        <strong className="mx-[0.5ch]">
+          TOTAL
+        </strong>
+        de los materiales que seleccionaste es de {numberToColombianPriceString(totalToShow)} de pesos colombianos.
+      </p>
+      <p>Si deseas verlo a detalle y recibir una asesoría personalizada solo
+        <strong className="mx-[0.5ch]">
+          ingresa los siguientes datos:
+        </strong>
+      </p>
+      <Input
+        onChange={onInputChange}
+        label="Nombre"
+        name="nombre"
+        value={data.nombre}
+        autoComplete="given-name"
+        required
+      />
+      {state.errors?.nombre && <p className="text-red-500">{state.errors.nombre._errors}</p>}
+      <Input
+        onChange={onInputChange}
+        label="Apellido"
+        name="apellido"
+        value={data.apellido}
+        autoComplete="family-name"
+        required
+      />
+      {state.errors?.apellido && <p className="text-red-500">{state.errors.apellido._errors}</p>}
+      <Input
+        onChange={onInputChange}
+        label="Email"
+        type="email"
+        name="email"
+        value={data.email}
+        autoComplete="email"
+        required
+      />
+      {state.errors?.email && <p className="text-red-500">{state.errors.email._errors}</p>}
+      <Input
+        onChange={onInputChange}
+        label="Teléfono"
+        name="telefono"
+        value={data.telefono}
+        autoComplete="tel"
+        required
+      />
+      {state.errors?.telefono && <p className="text-red-500">{state.errors?.telefono._errors}</p>}
+      <label className="flex flex-col gap-2">
+        <h4>
+          Mensaje (opcional):
+        </h4>
+        <textarea
+          onChange={onInputChange}
+          className={"px-2 py-1 rounded"}
+          value={data.mensaje}
+          name="mensaje"
+        />
+      </label>
+      {state.errors?.mensaje && <p className="text-red-500">{state.errors?.mensaje._errors}</p>}
+
+      <button formAction={formAction} className="bg-accent1 text-light px-4 sm:px-6 py-1.5 xs:py-2 text-sm sm:text-base w-fit self-end flex flex-col items-center justify-center rounded-lg text-nowrap flex-shrink-0 hover:-translate-y-0.5 hover:el-shadow">
+        Ver Cotización
+      </button>
+    </form>
+  );
+}
+
+export default CaptureForm;
+
+const Input = ({ type, className, label, ...rest }: ComponentPropsWithoutRef<"input"> & {
+  label: string;
+}) => {
+
+  return (
+    <label className="flex flex-col gap-2">
+      <h4>
+        {label}:
+      </h4>
+      <input {...rest} className={cn("px-2 py-1 rounded", className)} />
+    </label>
+  )
+}
--- app/(Pages)/cotizador/CaptureInfo.tsx
+++ app/(Pages)/cotizador/CaptureInfo.tsx
@@ -4,6 +4,7 @@ import { ComponentPropsWithoutRef, useEffect, useState } from "react";
 import LightCard from "../_components/LightCard";
 import { cn } from "../_lib/cn";
 import { colombianPriceStringToNumber, numberToColombianPriceString } from "@/app/helpers";
+import CaptureForm from "./CaptureForm";
 
 const CaptureInfo = ({
   createQueryString,
@@ -16,56 +17,52 @@ const CaptureInfo = ({
   const router = useRouter();
 
   useEffect(() => {
-    const surfaceTotals = Array.from(document.getElementsByClassName("surface-total"));
-    let total = 0;
-    surfaceTotals.forEach((surfaceTotal) => {
-      const textContent = surfaceTotal.textContent ?? '';
-      const price = colombianPriceStringToNumber(textContent);
-      total += price;
+    const surfaceTotals = document.getElementsByClassName("surface-total");
+    const observer = new MutationObserver(calculateTotal); // Create the observer
+
+    // Observe changes to the child list (nodes within the element) and character data (text content)
+    Array.from(surfaceTotals).forEach(surfaceTotal => {
+      observer.observe(surfaceTotal, { childList: true, characterData: true, subtree: true });
     });
-    setTotalToShow(total);
-  }, [captureInfoOpen]);
+
+    function calculateTotal() {
+      let total = 0;
+      Array.from(surfaceTotals).forEach(surfaceTotal => {
+        const textContent = surfaceTotal.textContent ? surfaceTotal.textContent.trim() : ''; // Trim whitespace!
+        const price = colombianPriceStringToNumber(textContent);
+        total += price;
+      });
+      setTotalToShow(total);
+    }
+
+    calculateTotal(); // Calculate initial total in case content is already there
+
+    return () => {
+      observer.disconnect(); // Clean up the observer on unmount
+    };
+  }, []);
   return (
     <>
-      <LinkButton scroll={false} text="Cotizar" color="naranja" size="mediano" link={`?${createQueryString("capture-info", "true", "add")}`} />
+      <LinkButton
+        // scroll={false}
+        text="Cotizar"
+        color="naranja"
+        size="mediano"
+        link={`?${createQueryString("capture-info", "true", "add")}`}
+      />
       {captureInfoOpen && (
         <section
           onClick={() => {
-            router.back()
+            router.push(`?${createQueryString("capture-info", "true", "remove")}`, { scroll: false })
           }}
-          className="fixed top-0 z-10 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
+          className="absolute top-0 z-[200] left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
         >
 
           <LightCard
             onClick={e => e.stopPropagation()}
             className="bg-light-dark p-10 relative z-[200] rounded-md px-10 flex flex-col w-[85dvw] mx-auto gap-5"
           >
-            <form className="flex flex-col gap-5">
-
-              <h1>¡Casi está lista tu cotización!</h1>
-              <p>El valor
-                <strong className="mx-[0.5ch]">
-                  TOTAL
-                </strong>
-                de los materiales que seleccionaste es de {numberToColombianPriceString(totalToShow)} de pesos colombianos.
-              </p>
-              <p>Si deseas verlo a detalle y recibir una asesoría personalizada solo
-                <strong className="mx-[0.5ch]">
-                  ingresa los siguientes datos:
-                </strong>
-              </p>
-              <Input label="Nombre" type="text" />
-              <Input label="Apellido" type="text" />
-              <Input label="Email" type="text" />
-              <Input label="Teléfono" type="text" />
-              <label className="flex flex-col gap-2">
-                <h4>
-                  Mensaje (opcional):
-                </h4>
-                <textarea className={"px-2 py-1 rounded"} />
-              </label>
-              <LinkButton text="Ver Cotización" color="naranja" size="mediano" link="/cotizador/confirmacion" className="w-fit self-end" />
-            </form>
+            <CaptureForm totalToShow={totalToShow} />
           </LightCard>
         </section>
       )}
@@ -76,15 +73,3 @@ const CaptureInfo = ({
 export default CaptureInfo;
 
 
-const Input = ({ className, label, ...rest }: ComponentPropsWithoutRef<"input"> & {
-  label: string;
-}) => {
-  return (
-    <label className="flex flex-col gap-2">
-      <h4>
-        {label}:
-      </h4>
-      <input {...rest} className={cn("px-2 py-1 rounded", className)} />
-    </label>
-  )
-}
