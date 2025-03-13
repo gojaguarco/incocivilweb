5c549941b78fe2343384091b58d9079d642a2880
Julian Bustos <julian.m.bustos@gmail.com>
2025-03-13 15:33:29 -0400

    adds console log to display surface details and calculated area in Cotizador

--- app/(Pages)/cotizador/Cotizador.tsx
+++ app/(Pages)/cotizador/Cotizador.tsx
@@ -40,6 +40,7 @@ const CotizadorUi = ({
       const area = surface?.formats
         ? (surface.formats[0].width * surface.formats[0].height) / 100
         : 0;
+      console.log({ surface, area, price: surface?.price });
       initialState[surfaceId] = {
         width: surface?.formats ? surface.formats[0].width : 0,
         height: surface?.formats ? surface.formats[0].height : 0,
