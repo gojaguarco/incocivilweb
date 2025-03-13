b069d6b29c976528eab255d1ea9ac85fc1648ca3
Julian Bustos <julian.m.bustos@gmail.com>
2025-03-13 15:44:44 -0400

    removes price formatting in console log for surface details in Cotizador

--- app/(Pages)/cotizador/Cotizador.tsx
+++ app/(Pages)/cotizador/Cotizador.tsx
@@ -32,8 +32,8 @@ const CotizadorUi = ({ surfaceTypes, catalogo }: {
     for (const surfaceId of selectedSurfaceIds) {
       const surface = catalogo.find((item) => item._id === surfaceId);
       const area = surface?.formats ? (surface.formats[0].width * surface.formats[0].height) / 100 : 0;
-      const price = Number(surface?.price?.replace(".", ""))
-      console.log({surface, area, price, totalSurf: area * price})
+      const price = surface?.price?.replace(".", "")
+      console.log({surface, area, price})
       initialState[surfaceId] = {
         width: surface?.formats ? surface.formats[0].width : 0,
         height: surface?.formats ? surface.formats[0].height : 0,
