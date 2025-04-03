3f7b0728db377b37b2bb6dfb97d925f114bd7827
Julian Bustos <julian.m.bustos@gmail.com>
2025-04-02 20:44:47 -0400

    feat: update Cotizador layout for improved responsiveness and structure

--- app/(Pages)/cotizador/page.tsx
+++ app/(Pages)/cotizador/page.tsx
@@ -17,15 +17,17 @@ const page = async () => {
   })
 
   return (
-    <section className="bg-light-dark md:bg-light w-full relative z-20 flex flex-col gap-5 py-10 px-5 md:px-10 max-w-6xl mx-auto">
-      <header>
-        <h1>
-          {cotizadorContent?.cotizador?.surfaceSelection?.title}
-        </h1>
-        <p className="font-montserrat">{cotizadorContent?.cotizador?.surfaceSelection?.subtitle}</p>
-      </header>
-      <Cotizador cotizadorContent={cotizadorContent} surfaceTypes={surfaceTypes} catalogo={catalogo} />
-    </section>
+    <div className="bg-light w-full z-10">
+      <section className="bg-light-dark md:bg-light min-h-screen w-full relative z-20 flex flex-col gap-5 py-10 px-5 md:px-10 max-w-6xl mx-auto">
+        <header>
+          <h1>
+            {cotizadorContent?.cotizador?.surfaceSelection?.title}
+          </h1>
+          <p className="font-montserrat">{cotizadorContent?.cotizador?.surfaceSelection?.subtitle}</p>
+        </header>
+        <Cotizador cotizadorContent={cotizadorContent} surfaceTypes={surfaceTypes} catalogo={catalogo} />
+      </section>
+    </div>
   );
 }
 
