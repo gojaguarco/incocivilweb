d42b3f1c84f9900e32faa353ce2be2bdc3c9aec8
Julian Bustos <64240466+julimancan@users.noreply.github.com>
2025-11-05 20:10:07 -0500

    adds the updated changes

--- app/(Pages)/cotizador/CaptureInfo.tsx
+++ app/(Pages)/cotizador/CaptureInfo.tsx
@@ -50,11 +50,11 @@ const CaptureInfo = ({
               { scroll: false }
             );
           }}
-          className="pt-10 fixed top-0 z-[100] left-0 w-screen h-full bg-black bg-opacity-50 flex justify-center items-center"
+          className="pt-10 fixed top-0 z-[50] left-0 w-screen h-full bg-black bg-opacity-50 flex justify-center items-center"
         >
           <LightCard
             onClick={(e) => e.stopPropagation()}
-            className="max-w-screen-sm bg-light-dark overflow-y-scroll md:overflow-y-auto max-h-[90svh] p-10 relative z-[200] rounded-xl px-10 flex flex-col w-[85dvw] mx-auto gap-5"
+            className="max-w-[500px] md:max-w-screen-sm bg-light-dark overflow-x-hidden overflow-y-scroll md:overflow-y-auto max-h-[70svh] p-10 relative z-[200] rounded-xl px-10 flex flex-col w-[85dvw] mx-auto gap-5"
           >
             <CaptureForm
               formTitle={formTitle}
